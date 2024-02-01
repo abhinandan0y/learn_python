7d620d4bfdfada0b847d9564d034f8c637d8d0ad
AbhinandanYadav <138775457+abhinandan0y@users.noreply.github.com>
2024-02-01 12:04:17 +0530

    Update README.md

--- Q_A Learn by Practice/README.m
+++ Q_A Learn by Practice/README.m
@@ -1,9 +1,9 @@
-  
-<div class="s-prose js-post-body" itemprop="text">
-<p>Find the group using Group model with the name of the group, then add the user to the user_set</p>
+# test  
+```
+Find the group using Group model with the name of the group, then add the user to the user_set
 
-<pre><code>from django.contrib.auth.models import Group
+<code>from django.contrib.auth.models import Group
 my_group = Group.objects.get(name='my_group_name') 
 my_group.user_set.add(your_user)
-</code></pre>
-    </div>
+</code>
+```
